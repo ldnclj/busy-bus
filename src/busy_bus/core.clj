c8a3772a015d1424cf78a2795054b28edd13c905
Hugo Firth <hugo.firth@gmail.com>
2017-04-10 20:38:07 +0100

    WIP

--- src/busy_bus/core.clj
+++ src/busy_bus/core.clj
@@ -22,16 +22,36 @@
         )))
 
 (defn create-new-passengers [world]
-  world)
+  (let [num-stops (count (:stops world))
+        passenger-location (rand-int num-stops)
+        passenger-destination (rand-nth
+                               (filter #(not (= passenger-location %))
+                                        (range num-stops)))]
+    (update-in world [:stops passenger-location] #(vec (conj % passenger-destination)))))
 
 (defn update-world [world location]
   (-> world
       (drop-and-pickup location)
       (create-new-passengers)))
 
-(defn strategy [world]
+(defn move-right [world]
   (mod (inc (:location (:bus world)))
        (count (:stops world))))
+
+(defn move-left [world]
+  (mod (dec (:location (:bus world)))
+       (count (:stops world)))))
+
+(defn move-stay [world]
+  world)
+
+(defn strategy-random [world]
+  (let [rand-num (rand-int 3)]
+    (cond
+      (= rand-num 0) (move-right world)
+      (= rand-num 1) (move-left world)
+      :else (move-stay world)
+      )))
 
 (defn main [world strategy num-steps]
   (loop [world world
@@ -40,3 +60,6 @@
       (let [new-location (strategy world)]
         (recur (update-world world new-location) (dec num-steps)))
       world)))
+
+
+#_(main world strategy-random 4)
